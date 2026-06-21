475018558294de53eb738815a60d9dde612c948d
sandygraphix <sandy.graphix@gmail.com>
2026-06-21 08:17:26 +0530

    Change logo import to logo-nav.svg

--- src/components/navigation/Navbar.tsx
+++ src/components/navigation/Navbar.tsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 import { Menu, X } from "lucide-react";
 import Image from "next/image";
 import PortfolioModal from "@/components/ui/PortfolioModal"; 
-import logo from "@/app/logo.svg";
+import logo from "@/app/logo-nav.svg";
 
 const navLinks = [
   { name: "Who We Are", href: "#about" },
